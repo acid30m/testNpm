1bf802c66c4ba6df6f0a5e2700def74123016256
Oleksandr Sinkevych <oleksandr.sinkevych@emotorwerks.com>
2023-02-05 14:07:33 +0200

    test123123

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
+import { defineConfig } from 'vite'
 
-export default {
+export default defineConfig ({
+  base: '/testNpm/',
   plugins: [
     handlebars({
       partialDirectory: resolve(__dirname, "partials"),
     }),
   ],
-};
+});
